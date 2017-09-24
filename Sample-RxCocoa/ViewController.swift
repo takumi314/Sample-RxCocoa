8a233b05093d82d3396eef6b49be0651fb5a4577
takumi314 <isospin_symmetry@me.com>
2017-09-24 23:48:48 +0900

    first commit

---
+++ Sample-RxCocoa/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Sample-RxCocoa
+//
+//  Created by NishiokaKohei on 2017/09/24.
+//  Copyright © 2017年 Kohey.N. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
