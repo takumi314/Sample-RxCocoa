9f50ffcd7820053a7c91c296e19552aa8e802745
takumi314 <isospin_symmetry@me.com>
2017-09-25 01:07:00 +0900

    make a UITableView

--- Sample-RxCocoa/MainViewController.swift
+++ Sample-RxCocoa/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  Sample-RxCocoa
 //
 //  Created by NishiokaKohei on 2017/09/24.
@@ -10,18 +10,18 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
 }
+
 
