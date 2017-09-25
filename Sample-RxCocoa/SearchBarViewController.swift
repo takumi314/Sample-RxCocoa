bb97937169675a7a19efb7e09779898c10bd5c0f
takumi314 <isospin_symmetry@me.com>
2017-09-25 22:50:06 +0900

    set-up UISearchBar and UITableView

--- Sample-RxCocoa/SearchBarViewController.swift
+++ Sample-RxCocoa/SearchBarViewController.swift
@@ -10,4 +10,39 @@ import UIKit
 
 class SearchBarViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+
+    var items = [String]()
+    var shownItems = ["London", "Paris", "Berlin", "Rome", "Madrid", "Bern"]
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    func setup() -> Void {
+
+    }
+    
+}
+
+extension SearchBarViewController: UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return  shownItems.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = shownItems[indexPath.row]
+
+        return cell
+    }
+
+
 }
