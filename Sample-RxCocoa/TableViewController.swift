9c4f6e54f3bdcb989b41cc2f37bcf78813c069d9
takumi314 <isospin_symmetry@me.com>
2017-09-25 22:01:21 +0900

    set-up UITableViewDataSource for TableViewController

--- Sample-RxCocoa/TableViewController.swift
+++ Sample-RxCocoa/TableViewController.swift
@@ -10,4 +10,36 @@ import UIKit
 
 class TableViewController: UIViewController {
     
+    @IBOutlet fileprivate weak var tableView: UITableView!
+
+    fileprivate var numbers = [1...30]
+    fileprivate var shownItems = [Int]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    func setup() -> Void {
+    }
+
+}
+
+extension TableViewController: UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shownItems.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = shownItems[indexPath.row].description
+        cell.detailTextLabel?.text = "hoge"
+
+        return cell
+    }
+
 }
