5c4086e7210b811f969590ab6ee850c2543259cc
takumi314 <isospin_symmetry@me.com>
2017-09-27 01:22:49 +0900

    set a observable for the UISlider

--- Sample-RxCocoa/SliderViewController.swift
+++ Sample-RxCocoa/SliderViewController.swift
@@ -14,20 +14,32 @@ class SliderViewController: UIViewController {
 
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var label: UILabel!
+
+    let disposeBug = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        setupRx()
     }
 
     func setupUI() -> Void {
         slider.maximumValue = 100
         slider.minimumValue = 0
-        slider.isContinuous = true
+        slider.value = 50
+        slider.minimumTrackTintColor = .blue
     }
 
     func setupRx() -> Void {
-        
+        slider.rx
+            .value
+            .distinctUntilChanged {
+                return abs($0 - $1) < 1
+            }
+            .subscribe { [unowned self] value in
+                self.label.text = Int(value.element!).description
+            }
+            .addDisposableTo(disposeBug)
     }
 
 }
