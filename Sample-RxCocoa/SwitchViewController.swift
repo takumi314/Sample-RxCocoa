b459a3ccb1817561a9e43aa2746badba7037a86c
takumi314 <isospin_symmetry@me.com>
2017-09-30 03:11:38 +0900

    setup autolayout and storyboard

--- Sample-RxCocoa/SwitchViewController.swift
+++ Sample-RxCocoa/SwitchViewController.swift
@@ -8,6 +8,20 @@
 
 import UIKit
 
-class SwitchViewController: UIViewController {
+class SwitchViewController: UITableViewController {
+
+    @IBOutlet weak var firstSwitch: UISwitch!
+    @IBOutlet weak var secondSwitch: UISwitch!
+    @IBOutlet weak var thirdSwitch: UISwitch!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    func setupUI() -> Void {
+
+    }
 
 }
+
+
