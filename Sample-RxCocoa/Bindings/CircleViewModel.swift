e2c0015522265ee1038dab3d60457493477659a0
takumi314 <isospin_symmetry@me.com>
2017-10-04 02:21:00 +0900

    create ViewModel class for the circleView

---
+++ Sample-RxCocoa/Bindings/CircleViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  CircleViewModel.swift
+//  Sample-RxCocoa
+//
+//  Created by NishiokaKohei on 2017/10/04.
+//  Copyright © 2017年 Kohey.N. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+import ChameleonFramework
+
+class PanGestureViewModel {
+
+}
+
+
