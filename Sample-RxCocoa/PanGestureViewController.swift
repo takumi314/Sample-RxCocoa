2db00280c6bb75c116b7ee530b67760b5abc7262
takumi314 <isospin_symmetry@me.com>
2017-10-04 01:43:05 +0900

    make PanGestureViewController class

---
+++ Sample-RxCocoa/PanGestureViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PanGestureViewController.swift
+//  Sample-RxCocoa
+//
+//  Created by NishiokaKohei on 2017/10/04.
+//  Copyright © 2017年 Kohey.N. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PanGestureViewController: UIViewController {
+
+}
