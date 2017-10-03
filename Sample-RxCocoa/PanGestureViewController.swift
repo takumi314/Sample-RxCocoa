9509afbb8fc2c2e00cbd181fd38099218e482513
takumi314 <isospin_symmetry@me.com>
2017-10-04 02:01:04 +0900

    draw a circle in the main view

--- Sample-RxCocoa/PanGestureViewController.swift
+++ Sample-RxCocoa/PanGestureViewController.swift
@@ -11,4 +11,20 @@ import UIKit
 
 class PanGestureViewController: UIViewController {
 
+    var circleView: UIView!
+
+    // MARK: - Life cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    func setupUI() -> Void {
+        circleView = UIView(frame: CGRect(origin: view.center, size: CGSize(width: 100.0, height: 100.0)))
+        circleView.layer.cornerRadius = circleView.frame.width / 2.0
+        circleView.center = view.center
+        circleView.backgroundColor = .green
+        view.addSubview(circleView)
+    }
+
 }
