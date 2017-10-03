7c8009dab59067a40da9f713bcdcc59ef0c185c3
takumi314 <isospin_symmetry@me.com>
2017-10-04 02:41:04 +0900

    setup Variable and Observer for ViewModel
    # Variable in a ViewModel will be used to calculate UI things,
    # every time this variable will get a new position, and will get calculated new ball’s background color

--- Sample-RxCocoa/Bindings/CircleViewModel.swift
+++ Sample-RxCocoa/Bindings/CircleViewModel.swift
@@ -11,7 +11,28 @@ import RxSwift
 import RxCocoa
 import ChameleonFramework
 
-class PanGestureViewModel {
+class CircleViewModel {
+
+    var centerVariable = Variable<CGPoint>(.zero)   // Create one variable that will be changed and observed
+    var backgroundColor: Observable<UIColor>!       // Create observable that will change backgroundColor based on center
+
+    init() {
+        setup()
+    }
+
+    func setup() -> Void {
+        // When we get new center, emit new UIColor
+        backgroundColor = centerVariable
+            .asObservable()
+            .map { center in
+                let red: CGFloat = (center.x + center.y).truncatingRemainder(dividingBy: 255.0) / 255.0 // We just manipulate red, but you can do w/e
+                let green: CGFloat = 0.0
+                let blue: CGFloat = 0.0
+
+                return UIColor.flatten(UIColor(red: red, green: green, blue: blue, alpha: 1.0))()
+            }
+
+    }
 
 }
 
